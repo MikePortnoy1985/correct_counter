d985f6f627d5833296684cb18a4ec0eed918a01f
Mike <m.temartsev@gmail.com>
2020-09-04 03:37:49 +0300

    Updates

---
+++ precache-manifest.a36ee4b4a556d1efa0532be09d19ce31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67a5a4ceae9953468fb3efe1d59e56a",
+    "url": "/MikePortnoy1985/correct_counter/index.html"
+  },
+  {
+    "revision": "a48944889801a997d2cd",
+    "url": "/MikePortnoy1985/correct_counter/static/css/main.2b3c54dd.chunk.css"
+  },
+  {
+    "revision": "d71c593a8918aff8a849",
+    "url": "/MikePortnoy1985/correct_counter/static/js/2.146f6a75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MikePortnoy1985/correct_counter/static/js/2.146f6a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48944889801a997d2cd",
+    "url": "/MikePortnoy1985/correct_counter/static/js/main.774572ad.chunk.js"
+  },
+  {
+    "revision": "a54d9acb730818a92d0e",
+    "url": "/MikePortnoy1985/correct_counter/static/js/runtime-main.dfe4d714.js"
+  }
+]);
